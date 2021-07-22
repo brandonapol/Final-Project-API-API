81da2846e13d50140fcb9846a27e2f5f8c2ed446
Brandon Apol <72466214+brandonapol@users.noreply.github.com>
2021-07-22 11:13:59 -0800

    Delete flask directory

--- flask/main/api/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, request, jsonify, redirect
-from helpers import token_required
-from models import db, User
-
-api = Blueprint('api',__name__, url_prefix='/api')
-
-@api.route('/getdata')
-def getdata():
-    return redirect ('https://stackoverflow.com/questions/18206630/how-can-i-redirect-to-another-url-and-pass-a-list-object-using-flask')
